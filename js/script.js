ebaec1b7af09bda0739853cb355ad20b67d407d1
Viktor <oliinyk.v.7.7@gmail.com>
2023-01-05 23:02:25 +0200

    HW2

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const storeFit = 'Cucumber';
+const storeDescription = {
+    budget : 10000,
+    employees : [`Lola`, `Mola`, `Hola`],
+    products : {
+        'tomato' : 10,
+        'cucumber' : 7
+    },
+    open: true
+}
